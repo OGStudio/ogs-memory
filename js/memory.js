c0492b5bc18b0e829101256463c478beb66ecb37
Михаил Капелько <kornerr@gmail.com>
2024-07-16 23:58:59 +0300

    d

---
+++ js/memory.js
@@ -0,0 +1,130 @@
+from memory_Context import *
+from cld import *
+
+# Detect mismatched items
+#
+# Conditions:
+# 0. Two items has just been selected
+# 1. The same item has been selected twice
+# 2. Selected items are of different groups
+@cld_by_value
+def memory_detectMismatchedItems(
+  c: memory_Context
+) -> memory_Context:
+  if not (
+    c.recentField == "selectedItems" and
+    len(c.selectedItems) == 2
+  ):
+    c.recentField = "none"
+    return c
+
+  if (
+    c.selectedItems[0] == c.selectedItems[1]
+  ):
+    c.mismatchedItems.clear()
+    c.mismatchedItems.append(c.selectedItems[0])
+    c.recentField = "mismatchedItems"
+    return c
+
+  if (
+    c.playfieldItems[c.selectedItems[0]] != c.playfieldItems[c.selectedItems[1]]
+  ):
+    c.mismatchedItems.clear()
+    c.mismatchedItems.append(c.selectedItems[0])
+    c.mismatchedItems.append(c.selectedItems[1])
+    c.recentField = "mismatchedItems"
+    return c
+
+  c.recentField = "none"
+  return c
+
+# Detect victory
+#
+# Conditions:
+# 1. Matching items have just been hidden and all items are hidden now
+@cld_by_value
+def memory_detectVictory(
+  c: memory_Context
+) -> memory_Context:
+  if (
+    c.recentField == "hiddenItems" and
+    len(c.hiddenItems) == len(c.playfieldItems)
+  ):
+    c.victory = True
+    c.recentField = "victory"
+    return c
+
+  c.recentField = "none"
+  return c
+
+# Generate constant playfield
+#
+# Conditions:
+# 1. Size has just been specified
+#
+# Both ids and group ids start with 0
+@cld_by_value
+def memory_generateConstPlayfield(
+  c: memory_Context
+) -> memory_Context:
+  if not (
+    c.recentField == "playfieldSize"
+  ):
+    c.recentField = "none"
+    return c
+
+  idGroups: dict[int, int] = { }
+  id = 0
+  for gid in range(0, c.playfieldSize):
+    idGroups[id] = gid
+    id += 1
+    idGroups[id] = gid
+    id += 1
+
+  c.playfieldItems = idGroups
+  c.recentField = "playfieldItems"
+  return c
+
+# Hide matching selected items
+#
+# Conditions:
+# 1. Two items are selected and they are of the same group
+@cld_by_value
+def memory_hideMatchingItems(
+  c: memory_Context
+) -> memory_Context:
+  if (
+    c.recentField == "selectedItems" and
+    len(c.selectedItems) == 2 and
+    c.playfieldItems[c.selectedItems[0]] == c.playfieldItems[c.selectedItems[1]]
+  ):
+    c.hiddenItems.append(c.selectedItems[0])
+    c.hiddenItems.append(c.selectedItems[1])
+    c.recentField = "hiddenItems"
+    return c
+
+  c.recentField = "none"
+  return c
+
+# Select item
+#
+# Conditions:
+# 1. Id has just been specified for selection
+@cld_by_value
+def memory_selectItem(
+  c: memory_Context
+) -> memory_Context:
+  if not (
+    c.recentField == "selectedId"
+  ):
+    c.recentField = "none"
+    return c
+
+  if (
+    len(c.selectedItems) == 2
+  ):
+    c.selectedItems.clear()
+
+  c.selectedItems.append(c.selectedId)
+  c.recentField = "selectedItems"
+  return c
---
+++ js/memory_test.js
@@ -0,0 +1,223 @@
+from memory import *
+from memory_Context import *
+
+def memory_test_detectMismatchedItems(
+) -> str:
+  c = memory_createContext()
+  c.playfieldSize = 2
+  c.recentField = "playfieldSize"
+  c = memory_generateConstPlayfield(c)
+
+  # Select two items of different groups.
+  c.selectedId = 0
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+  c.selectedId = 2
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+
+  # Detect mismatching.
+  c = memory_detectMismatchedItems(c)
+
+  # See if the two selected items do not match.
+  if (
+    c.recentField == "mismatchedItems" and
+    len(c.mismatchedItems) == 2 and
+    c.mismatchedItems[0] == 0 and
+    c.mismatchedItems[1] == 2
+  ):
+    return "OK: memory_detectMismatchedItems"
+
+  return "ERR: memory_detectMismatchedItems"
+
+def memory_test_detectMismatchedItems_itemTwice(
+) -> str:
+  c = memory_createContext()
+  c.playfieldSize = 2
+  c.recentField = "playfieldSize"
+  c = memory_generateConstPlayfield(c)
+
+  # Select the same item twice.
+  c.selectedId = 0
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+  c.selectedId = 0
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+
+  # Detect mismatching.
+  c = memory_detectMismatchedItems(c)
+
+  # See if the two selected items do not match.
+  if (
+    c.recentField == "mismatchedItems" and
+    len(c.mismatchedItems) == 1 and
+    c.mismatchedItems[0] == 0
+  ):
+    return "OK: memory_detectMismatchedItems_itemTwice"
+
+  return "ERR: memory_detectMismatchedItems_itemTwice"
+
+def memory_test_detectVictory(
+) -> str:
+  c = memory_createContext()
+  c.playfieldSize = 2
+  c.recentField = "playfieldSize"
+  c = memory_generateConstPlayfield(c)
+
+  # Select the first two items of the same group.
+  c.selectedId = 0
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+  c.selectedId = 1
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+
+  # Hide the first pair.
+  c = memory_hideMatchingItems(c)
+
+  # Select the last two items of the same group.
+  c.selectedId = 2
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+  c.selectedId = 3
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+
+  # Hide the second pair.
+  c = memory_hideMatchingItems(c)
+
+  # Detect victory.
+  c = memory_detectVictory(c)
+
+  # See if victory has been detected.
+  if (
+    c.recentField == "victory" and
+    c.victory == True
+  ):
+    return "OK: memory_detectVictory"
+
+  return "ERR: memory_detectVictory"
+
+def memory_test_generateConstPlayfield(
+) -> str:
+  c = memory_createContext()
+  c.playfieldSize = 2
+  c.recentField = "playfieldSize"
+  c = memory_generateConstPlayfield(c)
+  if (
+    c.recentField == "playfieldItems" and
+    len(c.playfieldItems) == 4 and
+    c.playfieldItems[0] == 0 and
+    c.playfieldItems[1] == 0 and
+    c.playfieldItems[2] == 1 and
+    c.playfieldItems[3] == 1
+  ):
+    return "OK: memory_generateConstPlayfield"
+
+  return "ERR: memory_generateConstPlayfield"
+
+def memory_test_hideMatchingItems(
+) -> str:
+  c = memory_createContext()
+  c.playfieldSize = 2
+  c.recentField = "playfieldSize"
+  c = memory_generateConstPlayfield(c)
+
+  # Select two items of the same group.
+  c.selectedId = 0
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+  c.selectedId = 1
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+
+  # Hide matching items.
+  c = memory_hideMatchingItems(c)
+
+  # See if the two selected items match.
+  if (
+    c.recentField == "hiddenItems" and
+    len(c.hiddenItems) == 2 and
+    c.hiddenItems[0] == 0 and
+    c.hiddenItems[1] == 1
+  ):
+    return "OK: memory_hideMatchingItems"
+
+  return "ERR: memory_hideMatchingItems"
+
+def memory_test_selectItem_1x(
+) -> str:
+  c = memory_createContext()
+  c.playfieldSize = 2
+  c.recentField = "playfieldSize"
+  c = memory_generateConstPlayfield(c)
+
+  # Select the first item.
+  c.selectedId = 0
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+
+  # See if it's in selectedItems now.
+  if (
+    c.recentField == "selectedItems" and
+    len(c.selectedItems) == 1 and
+    c.selectedItems[0] == 0
+  ):
+    return "OK: memory_selectItem_1x"
+
+  return "ERR: memory_selectItem_1x"
+
+def memory_test_selectItem_2x(
+) -> str:
+  c = memory_createContext()
+  c.playfieldSize = 2
+  c.recentField = "playfieldSize"
+  c = memory_generateConstPlayfield(c)
+
+  # Select the first two items.
+  c.selectedId = 0
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+  c.selectedId = 1
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+
+  # See if both items are selected now.
+  if (
+    c.recentField == "selectedItems" and
+    len(c.selectedItems) == 2 and
+    c.selectedItems[0] == 0 and
+    c.selectedItems[1] == 1
+  ):
+    return "OK: memory_selectItem_2x"
+
+  return "ERR: memory_selectItem_2x"
+
+def memory_test_selectItem_3x(
+) -> str:
+  c = memory_createContext()
+  c.playfieldSize = 2
+  c.recentField = "playfieldSize"
+  c = memory_generateConstPlayfield(c)
+
+  # Select three items.
+  c.selectedId = 0
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+  c.selectedId = 1
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+  c.selectedId = 2
+  c.recentField = "selectedId"
+  c = memory_selectItem(c)
+
+  # See if only one (last) item is selected now.
+  if (
+    c.recentField == "selectedItems" and
+    len(c.selectedItems) == 1 and
+    c.selectedItems[0] == 2
+  ):
+    return "OK: memory_selectItem_3x"
+
+  return "ERR: memory_selectItem_3x"
